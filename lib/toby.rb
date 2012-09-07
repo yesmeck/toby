a53cf065153590e198ecea3e8f5389add7837f4c
Meck <yesmeck@gmail.com>
2012-09-08 00:54:07 +0800

    使用 require_relative

--- lib/toby.rb
+++ lib/toby.rb
@@ -1,10 +1,8 @@
 module Toby
 end
 
-$: << '.'
-
-require 'lib/toby/client'
-require 'lib/toby/request/base'
-require 'lib/toby/request/item_get'
-require 'lib/toby/request/item_props_get'
-require 'lib/toby/util/request_check'
+require_relative 'toby/client'
+require_relative 'toby/request/base'
+require_relative 'toby/request/item_get'
+require_relative 'toby/request/item_props_get'
+require_relative 'toby/util/request_check'
