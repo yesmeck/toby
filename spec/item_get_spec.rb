290c65c90ae11c4b8c0cb1a8cd4ae54067e9a4e5
Meck <yesmeck@gmail.com>
2012-09-09 23:26:09 +0800

    把 spec 放到相应 module 下

--- spec/item_get_spec.rb
+++ spec/item_get_spec.rb
@@ -2,16 +2,23 @@
 
 require 'toby'
 
-describe "Item get" do
-  it "should returns a taobao item, when it is executed." do
-    Toby.app_key = '12350568'
-    Toby.secret_key = 'ebca8ce85d80e7c25d41f1d11fc308ab'
+module Toby
+  module Request
+    module Taobao
+      describe "Item get" do
+        it "should returns a taobao item, when it is executed." do
+          Toby.app_key = '12350568'
+          Toby.secret_key = 'ebca8ce85d80e7c25d41f1d11fc308ab'
 
-    client = Toby::Client.new
-    request = Toby::Request::Taobao::ItemGet.new
+          client = Toby::Client.new
+          request = Toby::Request::Taobao::ItemGet.new
 
-    request.num_iid = 13364998803
+          request.num_iid = 13364998803
 
-    client.execute(request).should have_key(:item)
+          client.execute(request).should have_key(:item)
+        end
+      end
+    end
   end
 end
+
--- spec/itemprops_get_spec.rb
+++ spec/itemprops_get_spec.rb
@@ -2,16 +2,22 @@
 
 require 'toby'
 
-describe 'Item properties get' do
-  it 'should returns item properties list' do
-    Toby.app_key = '12350568'
-    Toby.secret_key = 'ebca8ce85d80e7c25d41f1d11fc308ab'
+module Toby
+  module Requst
+    module Taobao
+      describe 'Item properties get' do
+        it 'should returns item properties list' do
+          Toby.app_key = '12350568'
+          Toby.secret_key = 'ebca8ce85d80e7c25d41f1d11fc308ab'
 
-    client = Toby::Client.new
-    request = Toby::Request::Taobao::ItempropsGet.new
+          client = Toby::Client.new
+          request = Toby::Request::Taobao::ItempropsGet.new
 
-    request.cid = 50012377
+          request.cid = 50012377
 
-    client.execute(request).should have_key(:item_props)
+          client.execute(request).should have_key(:item_props)
+        end
+      end
+    end
   end
 end
