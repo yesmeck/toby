1cd60cd143007fc81e1f7b1fc3f17be2368a3f37
Meck <yesmeck@gmail.com>
2012-09-10 22:14:20 +0800

    使用 spec_helper

--- spec/item_get_spec.rb
+++ spec/item_get_spec.rb
@@ -1,15 +1,12 @@
 # encoding: utf-8
 
-require 'toby'
+require 'spec_helper'
 
 module Toby
   module Request
     module Taobao
       describe "Item get" do
         it "should returns a taobao item, when it is executed." do
-          Toby.app_key = '12350568'
-          Toby.secret_key = 'ebca8ce85d80e7c25d41f1d11fc308ab'
-
           client = Toby::Client.new
           request = Toby::Request::Taobao::ItemGet.new
 
--- spec/itemprops_get_spec.rb
+++ spec/itemprops_get_spec.rb
@@ -1,15 +1,12 @@
 # encoding: utf-8
 
-require 'toby'
+require 'spec_helper'
 
 module Toby
   module Requst
     module Taobao
       describe 'Item properties get' do
         it 'should returns item properties list' do
-          Toby.app_key = '12350568'
-          Toby.secret_key = 'ebca8ce85d80e7c25d41f1d11fc308ab'
-
           client = Toby::Client.new
           request = Toby::Request::Taobao::ItempropsGet.new
 
