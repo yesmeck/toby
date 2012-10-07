fbfa26a6a97e385881708aaa500b4e626d82409f
Meck <yesmeck@gmail.com>
2012-10-07 20:10:22 +0800

    重写 taobao.taobaoke.shops.get 淘宝客店铺搜索

--- spec/toby/request/taobao/taobaoke_shops_get_spec.rb
+++ spec/toby/request/taobao/taobaoke_shops_get_spec.rb
@@ -1,18 +1,15 @@
 # encoding: utf-8
 
-module Toby
-  module Request
-    module Taobao
-      describe "Taobaoke shops get" do
-        it "should return a taoboke shops list." do
-          client = Toby::Client.new
-          request = Toby::Request::Taobao::TaobaokeShopsGet.new
+describe Toby::Request::Taobao::TaobaokeShopsGet do
+  include_context "initialize client"
 
-          request.keyword = "一米阳光官方旗舰店"
+  it "should return a taoboke shops list." do
+    request = Toby::Request::Taobao::TaobaokeShopsGet.new
+    request.keyword = "一米阳光官方旗舰店"
 
-          client.execute(request).should have_key(:taobaoke_shops)
-        end
-      end
-    end
+    @client.stub(:request).and_return(fixture("taobao/taobaoke_shops_get.json"))
+    shops = @client.execute(request)
+
+    shops.first.shop_title.should eq "一米阳光官方旗舰店"
   end
 end
