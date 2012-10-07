c714d54f7f726881aa71ac47ba52f7d3a1abd416
Meck <yesmeck@gmail.com>
2012-10-07 19:49:55 +0800

    重写 taobao.itemprops.get 获取标准商品类目属性

--- spec/toby/request/taobao/itemprops_get_spec.rb
+++ spec/toby/request/taobao/itemprops_get_spec.rb
@@ -1,20 +1,15 @@
 # encoding: utf-8
 
-require 'spec_helper'
+describe Toby::Request::Taobao::ItempropsGet do
+  include_context "initialize client"
 
-module Toby
-  module Requst
-    module Taobao
-      describe 'Item properties get' do
-        it 'should returns item properties list' do
-          client = Toby::Client.new
-          request = Toby::Request::Taobao::ItempropsGet.new
+  it 'should returns item properties list' do
+    request = Toby::Request::Taobao::ItempropsGet.new
+    request.cid = 50012377
 
-          request.cid = 50012377
+    @client.stub(:request).and_return(fixture("taobao/itemprops_get.json"))
+    item_props = @client.execute(request)
 
-          client.execute(request).should have_key(:item_props)
-        end
-      end
-    end
+    item_props.first.name.should eq "品牌"
   end
 end
