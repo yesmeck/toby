f9b20f9d921e6a6f77eb2607d935baf565cc0b92
Meck <yesmeck@gmail.com>
2012-10-26 09:47:13 +0800

    添加 taobao.taobaoke.items.get 测试

---
+++ spec/toby/request/taobao/taobaoke_items_get_spec.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+require "spec_helper"
+
+describe Toby::Request::Taobao::TaobaokeItemsGet do
+    include_context "initialize client"
+
+    it "should return a taobaoke istems list" do
+        request = Toby::Request.create("taobao.taobaoke.items.get")
+        request.cid = 50020808
+
+        @client.stub(:request).and_return(fixture("taobao/taobaoke_items_get.json"))
+
+        items = @client.execute(request)
+        items.first.num_iid.should eq 18083496676
+    end
+end
