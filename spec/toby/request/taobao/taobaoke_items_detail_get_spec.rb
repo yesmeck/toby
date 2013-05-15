51ad0cb0d9895f9aa3ddf016cc206fc721994ca2
Holin He <holin.he@b5m.com>
2013-05-15 16:08:33 +0800

    fix spec

--- spec/toby/request/taobao/taobaoke_items_detail_get_spec.rb
+++ spec/toby/request/taobao/taobaoke_items_detail_get_spec.rb
@@ -5,7 +5,7 @@ require "spec_helper"
 describe Toby::Request::Taobao::TaobaokeItemsDetailGet do
     include_context "initialize client"
 
-    it "should return a taobaoke istems detail info" do
+    it "should return a taobaoke items detail info" do
         request = Toby::Request.create("taobao.taobaoke.items.detail.get")
         request.fields = "num_iid,nick,price,click_url,shop_click_url,seller_credit_score,location"
         request.num_iids = "16231542746,22775120231"
@@ -13,7 +13,7 @@ describe Toby::Request::Taobao::TaobaokeItemsDetailGet do
         @client.stub(:request).and_return(fixture("taobao/taobaoke_items_detail_get.json"))
 
         items = @client.execute(request)
-        items.first.num_iid.should eq 22775120231
+        items.first.item.num_iid.should eq 22775120231
         items.total_results.should eq 2
     end
 end
